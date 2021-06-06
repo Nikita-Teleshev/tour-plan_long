623f5dbad1363803d396109947e44426b04eaccf
Nikita-Teleshev <83450537+Nikita-Teleshev@users.noreply.github.com>
2021-06-06 12:15:04 +0700

    create: section 'reviews'

--- js/main.js
+++ js/main.js
@@ -23,5 +23,15 @@ menuButton.on('click', function () {
   $('.navbar-bottom').toggleClass('navbar-bottom--visible');
 });
 
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  },
+});
    
  });
