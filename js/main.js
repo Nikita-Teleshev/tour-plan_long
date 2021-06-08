e132dd8a632248bcf5aa05396b70c7b2a2a7c616
Nikita-Teleshev <83450537+Nikita-Teleshev@users.noreply.github.com>
2021-06-08 13:01:51 +0700

    create: 'modal' window

--- js/main.js
+++ js/main.js
@@ -33,5 +33,23 @@ const reviewsSlider = new Swiper('.reviews-slider', {
     prevEl: '.reviews-slider__button--prev',
   },
 });
+
+var modalButton = $('[data-toggle=modal]');
+var closeModalButton = $('.modal__close');
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal)
+
+function openModal() {
+  var targetModal = $(this).attr('data-href');
+  $(targetModal).find('.modal__overlay').addClass('modal__overlay--visible');
+  $(targetModal).find('.modal__dialog').addClass('modal__dialog--visible');
+}
+function closeModal(event) {
+  event.preventDefault();
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.removeClass('modal__overlay--visible');
+  modalDialog.removeClass('modal__dialog--visible');
+}
    
  });
