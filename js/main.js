f4550bcf74d89d30630cf276a98bf42848592520
Nikita-Teleshev <83450537+Nikita-Teleshev@users.noreply.github.com>
2021-06-17 13:03:51 +0700

    create: validate

--- js/main.js
+++ js/main.js
@@ -51,5 +51,39 @@ function closeModal(event) {
   modalOverlay.removeClass('modal__overlay--visible');
   modalDialog.removeClass('modal__dialog--visible');
 }
+
+// Обработка форм
+  $('.form').each(function (){
+    $(this).validate({
+      errorClass: 'invalid',
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        phone: {
+          required: true,
+          minlength: 10
+        }
+      },
+      messages: {
+        name: {
+          required: 'Please specify your name',
+          minlength: 'Name must be at least 2 letters',
+        },
+        email: {
+          required: 'We need your email address to contact you',
+          email: 'Your email address must be in the format of name@domain.com',
+        },
+        phone: {
+          required: 'Your phone number is required',
+        },
+      },
+    });
+  });
    
  });
